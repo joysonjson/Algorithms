8e1177ad9a79a81d487ce54f93d0b6bdab3ac8b1
joysonjson <joysonpathra@gmail.com>
2022-06-04 21:20:45 +0530

    rotating right and left

--- rotate.playground/Contents.swift
+++ rotate.playground/Contents.swift
@@ -1,3 +1,39 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+// input array can be 1 to N
+// k 0 to N index to indicate the number of items to be rotated
+
+/*
+ -- space complexity O(1) because temp array created is fixed size wont increase dynamically
+ -- time complexity is O(n)
+**/
+func rotateRight<T>(array: [T], k: Int)->[T]{
+    guard !array.isEmpty || k > 0 else {
+        return array
+    }
+    
+    var temp = array
+    for i in 0..<array.count{
+        temp[(i+k) % array.count] = array[i]
+    }
+    return temp
+}
+
+func rotateLeft<T>(array: [T], k: Int)->[T]{
+    guard !array.isEmpty || k > 0 else {
+        return array
+    }
+    
+    var temp = array
+    for i in 0..<array.count{
+      temp[i] = array[(i+k) % array.count]
+    }
+    return temp
+}
+
+let res = rotateLeft(array: [1,2,3,4,5], k: 2)
+let res1 = rotateRight(array: [1,2,3,4,5], k: 2)
+
+print(res)
+print(res1)
